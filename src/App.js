9a57ae85c71c17f1a91c460a7bd1255d5de8f074
Dhanush V D Upadya <dhanushupadya@gmail.com>
2022-11-16 14:57:48 +0530

    Added Navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import {BrowserRouter as Router, Routes, Route}  from 'react-router-dom'
+import Explore from './pages/Explore';
+import Forgotpassword from './pages/Forgotpassword';
+import Navbar from './pages/Navbar';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+
+
 
 function App() {
   return (
    <>
-   <h1>Welcome</h1></>
+   <Router>
+    <Routes>
+      <Route path='/' element={<Explore/>}/>
+
+      <Route path='/forgotpassword' element={<Forgotpassword/>}/>
+
+      <Route path='/offers' element={<Offers/>}/>
+
+      <Route path='/profile' element={<Signin/>}/>
+
+      <Route path='/signin' element={<Signin/>}/>
+
+      <Route path='/signup' element={<Signup/>}/>
+    </Routes>
+
+    <Navbar/>
+   </Router>
+ </>
   );
 }
 
---
+++ src/pages/Forgotpassword.jsx
@@ -0,0 +1,12 @@
+
+function Forgotpassword() {
+    return (
+      <div>
+        <h1>Forgot password</h1>
+      </div>
+    )
+  }
+  
+  export default Forgotpassword
+  
+  
