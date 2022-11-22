c983a36761ce9be7d4689d15845d6690320f96f6
Dhanush V D Upadya <dhanushupadya@gmail.com>
2022-11-22 16:01:48 +0530

    Fetched single listing and its details

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,10 +1,77 @@
-
+import { useState, useEffect } from "react"
+import { Link,  useNavigate, useParams } from "react-router-dom"
+import {getDoc, doc} from 'firebase/firestore'
+import {getAuth} from 'firebase/auth'
+import { db } from "../firebase.config"
+import Spinner from "../components/Spinner"
+import shareIcon from '../assets/svg/shareIcon.svg'
+import { async } from "@firebase/util"
 
 function Listing() {
+
+    const [listing, setListing] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [sharedLinkCopied,setSharedLinkCopied] = useState(false)
+
+    const navigate = useNavigate()
+    const params = useParams()
+    const auth = getAuth()
+
+    useEffect(()=>{
+        const fetchListing = async ()=>{
+                const docRef = doc(db,'listings',params.listingId)
+                const docSnap = await getDoc(docRef)
+
+                if(docSnap.exists()){
+                    setListing(docSnap.data())
+                    setLoading(false)
+                }
+        }
+        fetchListing()
+    },[navigate,params.listingId])
+
+    if(loading){
+        return <Spinner/>
+    }
   return (
-    <div>
-      Listing
+    <main>
+        <div className="shareIconDiv" onClick={()=>{
+            navigator.clipboard.writeText(window.location.href)
+            setSharedLinkCopied(true)
+
+            setTimeout(()=>{
+                    setSharedLinkCopied(false)
+            },2000)
+        }}>
+
+            <img src={shareIcon} alt="ShareIcon" />
+
+        </div>
+
+        {sharedLinkCopied && <p className="linkCopied"> Link Copied!</p>}
+    <div className="listingDetails">
+        <p className="listingName">{listing.name} - ${listing.offer ?  listing.discountedPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g,',') :listing.regularPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g,',') }</p>
+    
+    <p className="listingLocation">{listing.location}</p>
+    <p className="listingType">
+        For {listing.type === 'rent' ? 'Rent' : 'Sale'}
+    </p>
+    {listing.offer && (
+    <p className="discountPrice">
+        ${listing.regularPrice- listing.discountedPrice} Discount</p>)}
+   
+   <ul className="listingDetailsList">
+    <li>
+        {listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms`: '1 Bedroom'}
+    </li>
+
+    <li>
+        {listing.bathrooms > 1 ? `${listing.bathrooms} Bathrooms`: '1 Bathroom'}
+    </li>
+   </ul>
+   
     </div>
+    </main>
   )
 }
 
